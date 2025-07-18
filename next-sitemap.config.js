bab57fad7e61b1f2272b12338a0d812631c0bb41
Yongjie Xie 谢师傅 <xieyongjie2012@163.com>
2025-07-18 01:55:26 -0400

    feat: remove next-sitemap dependency and add sitemap generation in sitemap.ts

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-
-module.exports = {
-  siteUrl: 'https://grokani.love/',
-  generateRobotsTxt: true,
-  changefreq:'weekly',
-  priority: 0.7,
-  generateIndexSitemap: false,
-}
